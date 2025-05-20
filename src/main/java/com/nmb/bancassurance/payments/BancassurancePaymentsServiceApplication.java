3c36ae9ab6d0e3045566b8e3968896727111f696
phijom <mtimbaphilip@gmail.com>
2025-05-21 01:19:44 +0300

    BANCA-331: (refactor) - update kafka service implementation

--- src/main/java/com/nmb/bancassurance/payments/BancassurancePaymentsServiceApplication.java
+++ src/main/java/com/nmb/bancassurance/payments/BancassurancePaymentsServiceApplication.java
@@ -1,13 +1,26 @@
 package com.nmb.bancassurance.payments;
 
+import com.nmb.bancassurance.shared.components.BaseServiceComponent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class BancassurancePaymentsServiceApplication {
+	@Value("${bancassurance.kafka.url}")
+	private String kafkaUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BancassurancePaymentsServiceApplication.class, args);
 	}
 
+	@Bean
+	protected BaseServiceComponent baseServiceComponent() {
+		return BaseServiceComponent.builder()
+				.kafkaUrl(kafkaUrl)
+				.build();
+	}
 }
--- src/main/java/com/nmb/bancassurance/payments/services/implementors/PaymentServiceImplementor.java
+++ src/main/java/com/nmb/bancassurance/payments/services/implementors/PaymentServiceImplementor.java
@@ -4,18 +4,22 @@ import com.nmb.bancassurance.payments.dtos.PaymentDTO;
 import com.nmb.bancassurance.payments.models.Payment;
 import com.nmb.bancassurance.payments.repository.PaymentRepository;
 import com.nmb.bancassurance.payments.services.PaymentService;
+import com.nmb.bancassurance.shared.components.BaseServiceComponent;
 import com.nmb.bancassurance.shared.dtos.APIResponder;
 import com.nmb.bancassurance.shared.dtos.PaginatedData;
 import com.nmb.bancassurance.shared.services.implementors.BaseServiceImplementor;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class PaymentServiceImplementor extends BaseServiceImplementor implements PaymentService {
     private final PaymentRepository paymentRepository;
+
+    public PaymentServiceImplementor(BaseServiceComponent baseServiceComponent, PaymentRepository paymentRepository) {
+        super(baseServiceComponent);
+        this.paymentRepository = paymentRepository;
+    }
 
     @Override
     public APIResponder.Paginated<PaymentDTO> fetchPayments(HttpServletRequest servletRequest) {
