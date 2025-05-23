4806a3c092e30434bb14c5bd0b48991b063cd412
phijom <mtimbaphilip@gmail.com>
2025-05-23 18:46:55 +0300

    BANCA-331: (chore) - remove kafka repo service implementation

--- src/main/java/com/nmb/bancassurance/payments/BancassurancePaymentsServiceApplication.java
+++ src/main/java/com/nmb/bancassurance/payments/BancassurancePaymentsServiceApplication.java
@@ -2,16 +2,15 @@ package com.nmb.bancassurance.payments;
 
 import com.nmb.bancassurance.shared.components.BaseServiceComponent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class BancassurancePaymentsServiceApplication {
-	@Value("${bancassurance.kafka.url}")
-	private String kafkaUrl;
+	private final KafkaTemplate<String, String> kafkaTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BancassurancePaymentsServiceApplication.class, args);
@@ -20,7 +19,7 @@ public class BancassurancePaymentsServiceApplication {
 	@Bean
 	protected BaseServiceComponent baseServiceComponent() {
 		return BaseServiceComponent.builder()
-				.kafkaUrl(kafkaUrl)
+				.kafkaTemplate(kafkaTemplate)
 				.build();
 	}
 }
